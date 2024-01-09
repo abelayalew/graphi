77c4d50011194013c1a02404384653e73c34e936
abelayalew <abelayalew81@gmail.com>
2024-01-10 01:54:10 +0300

    filters

--- graphi_crud/filters.py
+++ graphi_crud/filters.py
@@ -3,9 +3,15 @@ import graphene
 
 class StringFilterKeywordsInputType(graphene.InputObjectType):
     exact = graphene.String()
+    contains = graphene.String()
     icontains = graphene.String()
+    startswith = graphene.String()
     istartswith = graphene.String()
+    endswith = graphene.String()
     iendswith = graphene.String()
+    regex = graphene.String()
+    iregex = graphene.String()
+    isnull = graphene.Boolean()
 
 
 class DateFilterKeywordsInputType(graphene.InputObjectType):
@@ -14,4 +20,18 @@ class DateFilterKeywordsInputType(graphene.InputObjectType):
     lte = graphene.String()
     gt = graphene.String()
     gte = graphene.String()
+    range = graphene.List(graphene.String)
+    isnull = graphene.Boolean()
+    year = graphene.String()
+    month = graphene.String()
+    day = graphene.String()
+    week_day = graphene.String()
+    hour = graphene.String()
+    minute = graphene.String()
+    second = graphene.String()
+    date = graphene.String()
+    time = graphene.String()
+    iso_year = graphene.String()
+    iso_week = graphene.String()
+    iso_week_day = graphene.String()
 
