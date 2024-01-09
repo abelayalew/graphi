2e39be224dfc0341303626cf6784f62eceb16c6b
abelayalew <abelayalew81@gmail.com>
2024-01-10 00:40:34 +0300

    delete mutation generator

---
+++ graphi_crud/delete_mutation.py
@@ -0,0 +1,65 @@
+import graphene
+from .mutations import MutationsMixin
+from .queries import Queries
+
+
+class DeleteMutation(MutationsMixin):
+   
+    @classmethod
+    def generate_where_type(cls, model):
+        return Queries.generate_where_clause(model)
+
+    @classmethod
+    def generate_argument_class(cls, model):
+        where_type = cls.generate_where_type(model)
+        return type(
+            f'Arguments',
+            (),
+            {
+                "where": where_type,
+            }
+        )
+
+    @classmethod
+    def delete_mutate(cls, model):
+        def mutate(root, info, *args, **kwargs):
+            where = kwargs.get('where')
+
+            if not where:
+                raise Exception('there is no where clause')
+            
+            deleted_objects = []
+
+            queryset = model.objects.all()
+            query = Queries.query_set_builder(where)
+            queryset = queryset.filter(**query)
+            for obj in queryset:
+                obj.delete()
+                deleted_objects.append(obj)
+
+            return {
+                'affected_rows': len(deleted_objects)
+            }
+        return mutate
+
+    @classmethod
+    def generate_delete_mutation_class(cls, model):
+        argument_class = cls.generate_argument_class(model)
+        _ =  type(
+            f'{model.__name__}UpdateMutation',
+            (graphene.Mutation,),
+            {
+                'Arguments': argument_class,
+                'affected_rows': graphene.Int(),
+                'mutate': cls.delete_mutate(model)
+            }
+        )
+        return _
+
+    @classmethod
+    def generate_mutations(cls, _apps):
+        models = cls.find_all_models(_apps)
+        for model in models:
+            delete_mutation_class = cls.generate_delete_mutation_class(model)
+            setattr(cls, f'delete_{model.__name__.lower()}', delete_mutation_class.Field())
+
--- schema.py
+++ schema.py
@@ -3,16 +3,18 @@ import graphene
 from graphi_crud.queries import Queries
 from graphi_crud.create_mutation import CreateMutation
 from graphi_crud.update_mutation import UpdateMutation
+from graphi_crud.delete_mutation import DeleteMutation
 
 
 Queries.generate_queries(["accounts"])
 CreateMutation.generate_mutations(['accounts'])
 UpdateMutation.generate_mutations(['accounts'])
+DeleteMutation.generate_mutations(['accounts'])
 
 class Query(Queries):
     pass
 
-class Mutation(CreateMutation, UpdateMutation):
+class Mutation(CreateMutation, UpdateMutation, DeleteMutation):
     pass
 
 
